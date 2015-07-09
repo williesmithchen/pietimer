8f3c0621af4041899d42f2f41574366e0671376c
williesmithchen <williesmithchen@gmail.com>
2015-07-09 11:48:57 +0800

    Bug Fix

--- jquery.pietimer.js
+++ jquery.pietimer.js
@@ -103,7 +103,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
                     var seconds = Math.ceil(this.current_value/DEFAULT_VALUE * this.settings.seconds);
                     if(this.pieSeconds !== seconds){
                         this.pieSeconds = seconds;
-                        if(this.jquery_object.find(this.settings.elementID)) {
+                        if(this.jquery_object.find(this.settings.elementID).length !== 0) {
                             this.jquery_object.find(this.settings.elementID).text(this.pieSeconds);
                         }else{
                             $('#'+this.settings.elementID).html(this.pieSeconds);
